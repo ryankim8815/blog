bcf454e1b9188654e57b629d38a23a0185d814e9
Kim Byeongyong <ryankim8815@gmail.com>
2022-12-06 03:26:56 +0900

    [Feat]: socialLogin - added the feature for generating token

--- front/src/components/user/GoogleLogin.js
+++ front/src/components/user/GoogleLogin.js
@@ -1,17 +1,32 @@
 import React, { useState, useEffect, useContext } from "react";
 import * as Api from "../utils/Api";
 import { useNavigate } from "react-router-dom";
+import { DispatchContext } from "../../App";
 
 const GoogleLogin = () => {
   const navigate = useNavigate();
+  const dispatch = useContext(DispatchContext);
+
   const getPermissonCode = async () => {
     const params = new URL(window.location.href).searchParams;
     const code = params.get("code");
     const res = await Api.post("google", {
       code: code,
     });
-    console.log("res", res);
-    navigate("/");
+    if (res.data.result == false) {
+      alert("로그인에 실패하였습니다.\n");
+    } else {
+      const user = res.data;
+      const jwtToken = user.token;
+      sessionStorage.setItem("userToken", jwtToken);
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: user,
+      });
+      navigate("/");
+      window.location.reload();
+    }
   };
 
   useEffect(() => {
